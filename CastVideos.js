ec4f33daf6463eb98356e2b0f07b5c17d3813bce
Vincent Zhou <fireflysuccess@gmail.com>
2020-03-10 20:40:34 +0800

    Added my receiver id
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- CastVideos.js
+++ CastVideos.js
@@ -168,7 +168,7 @@ CastPlayer.prototype.initializeCastPlayer = function () {
   // Set the receiver application ID to your own (created in the
   // Google Cast Developer Console), or optionally
   // use the chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID
-  options.receiverApplicationId = 'C0868879';
+  options.receiverApplicationId = '80A6799C';
 
   // Auto join policy can be one of the following three:
   // ORIGIN_SCOPED - Auto connect from same appId and page origin
